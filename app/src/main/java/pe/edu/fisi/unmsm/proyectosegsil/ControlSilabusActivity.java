832acdd75c046d434efe9d2d985a2a217f629956
Ricardo Morales <ricindigus@gmail.com>
2018-05-06 01:07:48 -0500

    Creación de actividades principales

---
+++ app/src/main/java/pe/edu/fisi/unmsm/proyectosegsil/ControlSilabusActivity.java
@@ -0,0 +1,13 @@
+package pe.edu.fisi.unmsm.proyectosegsil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlSilabusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control_silabus);
+    }
+}
---
+++ app/src/main/java/pe/edu/fisi/unmsm/proyectosegsil/SilabusMenuActivity.java
@@ -0,0 +1,13 @@
+package pe.edu.fisi.unmsm.proyectosegsil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SilabusMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_silabus_menu);
+    }
+}
